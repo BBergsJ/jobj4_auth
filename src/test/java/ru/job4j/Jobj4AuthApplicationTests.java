bcf3ac8ed80c5adf3ce42f3373108e557a1cc2be
BBergsJ <goldlike888@gmail.com>
2022-01-27 11:33:55 +0300

    create job4j_auth

---
+++ src/test/java/ru/job4j/Jobj4AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jobj4AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
